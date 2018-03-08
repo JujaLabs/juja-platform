8ed10594e940bdd62334e0f9a5593b6ae6d5f9c3
Valerii <valerii.fedorenko.ua@gmail.com>
2018-03-08 13:42:03 +0300

    #32 add class ChannelServiceImpl(implementation of ChannelService)

---
+++ slack-archive/src/main/java/juja/microservices/slack/archive/service/impl/ChannelServiceImpl.java
@@ -0,0 +1,28 @@
+package juja.microservices.slack.archive.service.impl;
+
+import juja.microservices.slack.archive.model.Channel;
+import juja.microservices.slack.archive.repository.ChannelRepository;
+import juja.microservices.slack.archive.service.ChannelService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Service
+@Slf4j
+public class ChannelServiceImpl implements ChannelService {
+
+    @Inject
+    private ChannelRepository channelRepository;
+
+    @Override
+    public List<Channel> getChannels() {
+        return channelRepository.getChannels();
+    }
+
+    @Override
+    public void saveChannel(Channel channel) {
+        channelRepository.saveChannel(channel);
+    }
+}
